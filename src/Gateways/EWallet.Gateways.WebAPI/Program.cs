721b16209b8f2ba7d79ed928c3aaafcba6b439a5
taidangduc <taidangduc1503@gmail.com>
2026-06-27 20:43:42 +0700

    feat: add simple ratelimit and healthcheck in apigateway

--- src/Gateways/EWallet.Gateways.WebAPI/Program.cs
+++ src/Gateways/EWallet.Gateways.WebAPI/Program.cs
@@ -1,8 +1,16 @@
+using System.Security.Claims;
+using System.Threading.RateLimiting;
 using EWallet.Common.Web;
 using EWallet.Gateways.WebAPI.ConfigurationOptions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 //ref: https://code-maze.com/aspnetcore-api-gateway-with-ocelot
 //ref: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/yarp/config-files?view=aspnetcore-10.0
+//ref: https://learn.microsoft.com/en-us/aspnet/core/performance/rate-limit?view=aspnetcore-10.0
+//ref: https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-10.0
+//ref: https://www.c-sharpcorner.com/article/rate-limiting-and-throttling-in-asp-net-core-web-api/
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -30,6 +38,36 @@ builder.Services.AddCustomJwtAuthentication(appSettings.Jwt);
 
 builder.Services.AddAuthorization();
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    options.OnRejected = async (context, cancellationToken) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        await context.HttpContext.Response.WriteAsync("Too many requests.", cancellationToken);
+    };
+
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+    {
+        // You can bind rate limits to users, API keys, or IPs.
+        // NOTE: use ForwardedHeaders middleware if you are behind a reverse proxy.
+        var partitionKey =
+            httpContext.User?.FindFirst("sub")?.Value ??
+            httpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value ??
+            "anonymous";
+
+        return RateLimitPartition.GetFixedWindowLimiter(partitionKey, _ => new FixedWindowRateLimiterOptions
+        {
+            PermitLimit = 50,
+            Window = TimeSpan.FromMinutes(1),
+            QueueLimit = 0,
+        });
+    });
+});
+
+builder.Services.AddHealthChecks();
+
 builder.Services.AddReverseProxy()
     .LoadFromConfig(configuration.GetSection("Yarp"));
 
@@ -40,6 +78,10 @@ app.UseCors(appSettings.CORS.AllowAnyOrigin ? "AllowOrigin" : "AllowOrigins");
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseRateLimiter();
+
+app.MapHealthChecks("/health");
 
 app.MapReverseProxy();
 
